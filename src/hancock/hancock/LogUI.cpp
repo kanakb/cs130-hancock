996e8f91e572233546216a565afc0f92296ac5f6
Dasajed <Dasajed@1f8f3222-2881-11de-bcab-dfcfbda92187>
2009-05-17 22:49:38 +0000

    Added sort functionality to logui.

--- src/hancock/hancock/LogUI.cpp
+++ src/hancock/hancock/LogUI.cpp
@@ -12,7 +12,9 @@
 
 
 
+
 // LogUI dialog
+bool cmpFileNames(CString cs1, CString cs2);
 
 IMPLEMENT_DYNAMIC(LogUI, CDialog)
 
@@ -34,11 +36,16 @@ void LogUI::DoDataExchange(CDataExchange* pDX)
 	DDX_Control(pDX, IDC_LOGTEXT, m_logText);
 
 	//Populates Left Pane with files in log folder
-	CString s[256];
-	int n = this->getLogFiles(s);
-	for (int i = 0; i < n; i++){
-		m_logList.InsertString(i,s[i]);
+	list<CString> names;
+	list<CString>::iterator it;
+	names = this->getLogFiles();
+	names.sort(cmpFileNames);
+	int i=0;
+	for (it=names.begin(); it!=names.end(); ++it){
+		CString cs = *it;
+		m_logList.InsertString(i,cs);
 		m_logList.SetCurSel(i);
+		i++;
 	}
 	
 	//Populates Main Pane with Current Log File
@@ -99,8 +106,10 @@ void LogUI::PostNcDestroy() {
 	//delete this;
 }
 
-int LogUI::getLogFiles(CString* fileList){
+list<CString> LogUI::getLogFiles(){
 	
+	list<CString> fileList;
+
 	struct _finddatai64_t data;
 	// First create the filename that will be use to initialize the find.
 	// "*.*" are wild card characters that tells the find function to return a
@@ -114,19 +123,45 @@ int LogUI::getLogFiles(CString* fileList){
 	// start the finder -- on error _findfirsti64() will return -1, otherwise if no
 	// error it returns a handle greater than -1.
 	long h = _findfirsti64(fname.c_str(),&data);
-	int n = 0; //Size of array
 	if (h>0){
 		
 		do{
-			fileList[n] = data.name;
-			n++;
+			const CString cs(data.name);
+			fileList.push_back(cs);
 		}while( _findnexti64(h,&data) == 0);
 		
 		_findclose(h);
 	
 	}
 
-	return n;
+	return fileList;
 
+}
+bool cmpFileNames(CString cs2, CString cs1){
+	//compares year
+	for (int i = 6; i < 10; i++){
+		if (cs1[i]>cs2[i])
+			return true;
+		else if (cs1[i]<cs2[i])
+			return false;
+	}
+
+	//compares month, day
+	for (int i = 0; i < 6; i++){
+		if (cs1[i]>cs2[i])
+			return true;
+		else if (cs1[i]<cs2[i])
+			return false;
+	}
+
+	//compares hours, minutes, milliseconds
+	for (int i = 11; i < 19; i++){
+		if (cs1[i]>cs2[i])
+			return true;
+		else if (cs1[i]<cs2[i])
+			return false;
+	}
+	
+	return true;
 }
 	
