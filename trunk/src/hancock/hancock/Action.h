f2784a24481c71b6a8a547c3d928a100c64385fe
mihae.wilson <mihae.wilson@1f8f3222-2881-11de-bcab-dfcfbda92187>
2009-05-20 06:56:59 +0000

    Untested Action class. We decided to have 2 constructors differentiate between the standard and non-standard actions.

--- trunk/src/hancock/hancock/Action.h
+++ trunk/src/hancock/hancock/Action.h
@@ -4,30 +4,69 @@
  * 
  */
 
+
 #ifndef ACTION_H
 #define ACTION_H
+
 #include "MPane.h"
 #include <list>
 #include <string>
+#include "windows.h"
+#include "stdio.h"
+#include <tchar.h>
 using namespace std;
+
 
 /*** Define Status codes here ***/
 
-class Action {
 
+class Action {
+	
+	
+	
 public:
+	
+	Action();
+	
+	Action(string path, string name, string cfg);	//used for most actions
+	
+	Action(string path, string name, vector<string> &params); //the last parameter is a vector of arguments for the executable.
+	
 	virtual void act();					//builds the cmd and runs the action
+	
 	virtual string getName();			//returns the name of the executable
+	
 	virtual bool isComplete();			//tells if process is done
 	
-	list<string> m_inputs;				//list of input files  (full path)
-	list<string> m_outpus;				//list of output files (full path)
+	virtual int getStatus();
+	
+	
+	
 	string exePath;						//Location of the executable
+	
 	string exeName;						//Some way to refer to executable. 
+	
 	string m_symantecCfg;				//Path of the .cfg file used as input for the executable
+	
 	int status;							//status of action
 	
+	
+	
+protected:
+	
+	void executeProcess(string cmd);
+	
+	PROCESS_INFORMATION process_info;
+
+	vector<string> argv;
+
+	
+
 };
 
 
+
 #endif ACTION_H
+
+
+
