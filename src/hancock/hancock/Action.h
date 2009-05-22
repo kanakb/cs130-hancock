97bb6cefacc1a3cf49b1157a50f85e8072100116
mihae.wilson <mihae.wilson@1f8f3222-2881-11de-bcab-dfcfbda92187>
2009-05-22 02:28:00 +0000

    Temporary Action class added. Runs whatever executable is hard coded in Action::executeProcess(string cmd), but still needs to handle the type conversion from string to LPTSTR in this function.

--- src/hancock/hancock/Action.h
+++ src/hancock/hancock/Action.h
@@ -8,12 +8,17 @@
 #ifndef ACTION_H
 #define ACTION_H
 
-#include "MPane.h"
-#include <list>
+#define MAX_BUF_LEN 1024
+
+#include "MPane.h" 
+#include <iostream>
+#include <vector>
 #include <string>
 #include "windows.h"
 #include "stdio.h"
 #include <tchar.h>
+#include "stdafx.h"
+//#include <curses.h>	// !!!!TEMPORARY!!!!
 using namespace std;
 
 
@@ -22,51 +27,31 @@ using namespace std;
 
 class Action {
 	
-	
-	
 public:
-	
 	Action();
-	
 	Action(string path, string name, string cfg);	//used for most actions
-	
 	Action(string path, string name, vector<string> &params); //the last parameter is a vector of arguments for the executable.
-	
 	virtual void act();					//builds the cmd and runs the action
-	
 	virtual string getName();			//returns the name of the executable
-	
 	virtual bool isComplete();			//tells if process is done
-	
 	virtual int getStatus();
 	
-	
-	
 	string exePath;						//Location of the executable
-	
 	string exeName;						//Some way to refer to executable. 
-	
 	string m_symantecCfg;				//Path of the .cfg file used as input for the executable
-	
 	int status;							//status of action
-	
-	
-	
+	string output;
+
 protected:
-	
 	void executeProcess(string cmd);
-	
-	PROCESS_INFORMATION process_info;
-
-	vector<string> argv;
-
-	
-
+	char buf[MAX_BUF_LEN];		// i/o buffer
+	HANDLE newstdin,newstdout,read_stdout,write_stdin;
+	PROCESS_INFORMATION pi;
+	vector<string> argv;	
 };
 
-
-
 #endif ACTION_H
+
 
 
 
