294a711d6dc507d61bd14b02e70e9b71b7103517
mihae.wilson <mihae.wilson@1f8f3222-2881-11de-bcab-dfcfbda92187>
2009-05-22 02:28:00 +0000

    Temporary Action class added. Runs whatever executable is hard coded in Action::executeProcess(string cmd), but still needs to handle the type conversion from string to LPTSTR in this function.

--- trunk/src/hancock/hancock/Action.cpp
+++ trunk/src/hancock/hancock/Action.cpp
@@ -4,205 +4,115 @@
  *
  */
 
-#include "stdafx.h"
+#include "stdafx.h" 
 #include "Action.h"
 
-
-
 Action::Action()
-
 {
-	
 	exeName = "";
-	
 	exePath = "";
-	
 	m_symantecCfg = "";
-	
-	status = -1;
-	
-	
-	
+	status = -1;	
 }
-
-
 
 Action::Action(string path, string name, vector<string> &params)
-
 {
-	
 	exeName = name;
-	
 	exePath = path;
-	
 	m_symantecCfg = "";
-	
 	status = -1;
-	
 	argv = params;
-	
-	
-	
 }
-
-
 
 Action::Action(string path, string name, string cfg)
-
 {
-	
 	exeName = name;
-	
 	exePath = path;
-	
 	m_symantecCfg = cfg;
-	
 	status = -1;
-	
-	
-	
 }
-
-
-
-
 
 void Action::executeProcess(string cmd){
 	
-	//Optionally check for exe existance
+	//Optionally check for exe existance	
+	STARTUPINFO si;	
+    //PROCESS_INFORMATION pi;		
 	
-	
-	
-	STARTUPINFO si;
-	
-    PROCESS_INFORMATION pi;
-	
-	
-	
-    ZeroMemory( &si, sizeof(si) );
-	
-    si.cb = sizeof(si);
-	
-    ZeroMemory( &pi, sizeof(pi) );
-	
-	
-	
-	if( !CreateProcess( NULL,								// Module name 
-					   
-					   LPWSTR(cmd.c_str()),										// Command line
-					   
-					   NULL,												// Process handle
-					   
-					   NULL,												// Thread handle
-					   
-					   FALSE,												// Set handle inheritance to FALSE
-					   
-					   0,													// No creation flags
-					   
-					   NULL,												// Use parent's environment block
-					   
+    ZeroMemory( &si, sizeof(si) );	
+    si.cb = sizeof(si);	
+    ZeroMemory( &pi, sizeof(pi) );	
+
+	LPTSTR szCmdline = _tcsdup(TEXT("Z:\\test.exe"));
+
+	if( !CreateProcess( NULL,												// Module name 					   
+					   szCmdline,									// Command line					   
+					   NULL,												// Process handle					   
+					   NULL,												// Thread handle					   
+					   FALSE,												// Set handle inheritance to FALSE					   
+					   0,													// No creation flags					   
+					   NULL,												// Use parent's environment block					   
 					   NULL,												// Use parent's starting directory 
-					   
-					   &si,												// Pointer to STARTUPINFO structure
-					   
+					   &si,													// Pointer to STARTUPINFO structure
 					   &pi )												// Pointer to PROCESS_INFORMATION structure
-	   
-	   )
-		
+	   )	
 	{
-		
 		//Create Process failed.
-		
-	}
-	
-	process_info = pi;
-	
+		cout<<"Create Process Failed! Error:"<<GetLastError()<<endl;
+	}	
+
+
+
+
 }
 
-
-
 void Action::act()
-
 {
-	
-	if(!m_symantecCfg.length())
-		
-		executeProcess(exePath+exeName+" "+m_symantecCfg);
-	
+	if(m_symantecCfg.length())
+		executeProcess("\""+exePath+exeName+"\""+" "+m_symantecCfg);
 	else
-		
 	{
-		
 		string cmd = "";
-		
 		for(vector<string>::iterator it = argv.begin(); it != argv.end(); it++)
-			
-		{
-			
-			cmd+=(" " + *it);
-			
+		{	
+			cmd+=(" " + *it);	
 		}
-		
-		executeProcess(exePath+exeName+cmd);
-		
+		executeProcess("\""+exePath+exeName+"\""+cmd);
 	}
-	
 }
 
 
 
 bool Action::isComplete()
-
 {
-	
-	DWORD retCode = 0;
-	
-	GetExitCodeProcess(process_info.hProcess, LPDWORD(retCode));
-	
-	
-	
-	if(retCode == STILL_ACTIVE)
-		
+	DWORD retCode = 0;	
+	if(!GetExitCodeProcess(pi.hProcess, &retCode))
 	{
-		
-		return false;
-		
+		cout<<"Could not get Exit Code! Error: "<<GetLastError()<<endl;
 	}
 	
+	if(retCode == STILL_ACTIVE)		
+	{
+		return false;		
+	}
 	
+	cout<<endl<<"Done!!!!"<<endl;
+	status = retCode;	
+	// Close process and thread handles.
 	
-	status = retCode;
-	
-	// Close process and thread handles. 
-	
-	CloseHandle( process_info.hProcess );
-	CloseHandle( process_info.hThread );
-
+	CloseHandle( pi.hProcess );
+	CloseHandle( pi.hThread );
 
 	return true;
 
-
-
-
-
+	
 }
-
-
 
 int Action::getStatus()
-
 {
-
 	return status;
-
 }
 
-
-
 string Action::getName()
-
 {
-
 	return exeName;
-
 }
